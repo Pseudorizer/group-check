344dbf3c1e973f790244616463ad1352c800d049
Emily Medhurst <emily.medhurst2002@protonmail.com>
2023-08-23 23:53:39 +0100

    add webfont plugin

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import mkcert from 'vite-plugin-mkcert';
 import legacy from '@vitejs/plugin-legacy';
 import dynamicImport from 'vite-plugin-dynamic-import';
 import AutoImport from 'unplugin-auto-import/vite';
+import webfontDownload from 'vite-plugin-webfont-dl';
 
 export default defineConfig({
   server: {
@@ -14,6 +15,9 @@ export default defineConfig({
     react(),
     legacy(),
     dynamicImport(),
+    webfontDownload([
+      'https://fonts.googleapis.com/css2?family=Atkinson+Hyperlegible:wght@400;700&display=swap',
+    ]),
     AutoImport({
       dts: true,
       eslintrc: {
